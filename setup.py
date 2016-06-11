86e20325278cac20db6b33389ed59962d1f7ee10
Michael Merickel <michael@merickel.org>
2016-06-11 15:41:34 -0500

    add a license

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ except IOError:
 
 install_requires = [
     'PasteDeploy >= 1.5.0',  # py3 compat
+    'plaster',
 ]
 
 docs_require = [
@@ -30,9 +31,12 @@ tests_require = [
 setup(
     name='plaster_pastedeploy',
     version='0.1.0',
-    description="A loader implementing the PasteDeploy loading syntax to be used by plaster.",
+    description=(
+        'A loader implementing the PasteDeploy loading syntax to be used '
+        'by plaster.'
+    ),
     long_description=readme + '\n\n' + changes,
-    author="Hunter Senft-Grupp",
+    author='Hunter Senft-Grupp',
     author_email='huntcsg@gmail.com',
     url='https://github.com/mmerickel/plaster_pastedeploy',
     packages=find_packages(exclude=['tests']),
@@ -42,14 +46,15 @@ setup(
         'docs': docs_require,
         'testing': tests_require,
     },
-    test_suite="tests",
+    test_suite='tests',
     zip_safe=False,
     keywords='plaster pastedeploy plaster_pastedeploy ini config',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
@@ -57,8 +62,9 @@ setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
-    entry_points="""\
-    [plaster.loader]
-    ini+pastedeploy=plaster_pastedeploy:Loader
-    """
+    entry_points={
+        'plaster.loader': [
+            'ini+pastedeploy=plaster_pastedeploy:Loader',
+        ],
+    },
 )
