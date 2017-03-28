ecd8a2afb058689ba3d7243fa6ef9922a1ac1819
Michael Merickel <michael@merickel.org>
2017-03-27 22:54:30 -0500

    support python_requires for pip 9+

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
     packages=find_packages('src', exclude=['tests']),
     package_dir={'': 'src'},
     include_package_data=True,
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     install_requires=install_requires,
     extras_require={
         'testing': tests_require,
