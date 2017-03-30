27aa8812d3855a3bb22e2f4f391d7d2b5ab6f542
Michael Merickel <michael@merickel.org>
2017-03-30 02:05:22 -0500

    update to beta

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     zip_safe=False,
     keywords='plaster pastedeploy plaster_pastedeploy ini config egg',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
